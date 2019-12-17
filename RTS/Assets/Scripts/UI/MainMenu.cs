f3cabd2637df16161b752b4be1cbe41daa246156
daniel_k_m <daniel.marshallk@gmail.com>
2019-12-16 22:01:28 -0500

    Fixed scene management

--- RTS/Assets/Scripts/UI/MainMenu.cs
+++ RTS/Assets/Scripts/UI/MainMenu.cs
@@ -9,6 +9,7 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
     public void QuitGame()
     {
         Debug.Log("Quit!");
