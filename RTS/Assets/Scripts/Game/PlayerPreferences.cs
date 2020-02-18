e94c08b91b4b8585b84d902eecad7a947b9fb181
daniel_k_m <daniel.marshallk@gmail.com>
2020-02-18 18:45:12 -0500

    Added player preferences (saves volume)

---
+++ RTS/Assets/Scripts/Game/PlayerPreferences.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPreferences : MonoBehaviour
+{
+    [SerializeField]
+    private Slider volumeSlider;
+    private float volume;
+
+    string userName = "traveller";
+    // Start is called before the first frame update
+
+    public void Awake() {
+        LoadPlayerPreferences();
+    }
+
+    public void ApplyPlayerPreferences()
+    {
+        volume = volumeSlider.value;
+        PlayerPrefs.SetFloat("volume", volume);
+        PlayerPrefs.SetString("username", userName);
+        PlayerPrefs.Save();
+        Debug.Log("Game data saved!");
+    }
+
+    public void LoadPlayerPreferences()
+    {
+        if (PlayerPrefs.HasKey("username"))
+        {
+            volume = PlayerPrefs.GetFloat("volume");
+            volumeSlider.value = PlayerPrefs.GetFloat("volume");
+            userName = PlayerPrefs.GetString("username");
+            Debug.Log("Game data loaded!");
+        }
+        else
+        {
+            Debug.LogError("There is no save data!");
+        }
+    }
+    
+    public void ResetPlayerPreferences()
+    {
+        PlayerPrefs.DeleteAll();
+        volume = 100.0f;
+        volumeSlider.value = 100.0f;
+        userName = "traveller";
+        Debug.Log("Data reset complete");
+    }
+}
