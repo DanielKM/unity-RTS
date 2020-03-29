737cb931b6200f8e779a5746bb5fa2d53a185aa1
daniel_k_m <daniel.marshallk@gmail.com>
2020-03-29 15:50:36 -0400

    Added daily summoning for necromancers

---
+++ RTS/Assets/Scripts/Unit Scripts/NecromancerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NecromancerController : MonoBehaviour
+{
+    // UI Variables
+    public GameObject boneshards;
+    
+    DayNightCycle DayNight;
+    public GameObject faction;
+    public GameObject MainCamera;
+    public Transform TownCenter;
+
+    public GameObject skeleton;
+    public bool raisingDead = false;
+
+    private Vector3 spawnPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        faction = GameObject.Find("Faction");
+        MainCamera = GameObject.Find("Main Camera");
+        DayNight = MainCamera.GetComponent<DayNightCycle>();
+        TownCenter = GameObject.Find("TownCenter").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameObject.GetComponent<UnitController>().isDead) {
+            if(DayNight.time >= 68000 && DayNight.time <= 68500 ) {
+                if(!raisingDead) {
+                    Debug.Log(DayNight.days);
+                    StartCoroutine(RaiseDead(DayNight.days));
+                }          
+            }
+        }
+    }
+
+    IEnumerator RaiseDead(int number) {
+        raisingDead = true;
+        yield return new WaitForSeconds(10);
+        spawnPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - 5f);
+        Transform[] loadedTransforms = new Transform[1];
+        loadedTransforms[0] = TownCenter;
+        for(int i=0; i<number + 1; i++) {
+            skeleton.GetComponent<NPCController>().waypoints = loadedTransforms;
+            Instantiate(skeleton, spawnPosition, Quaternion.identity);  
+            raisingDead = false;
+        }
+    }
+}
